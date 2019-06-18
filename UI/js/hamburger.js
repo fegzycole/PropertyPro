fd1a2b01f77761a37a2bbe67abac21b7a2d74b88
Iyara Ferguson <fergusoniyara@gmail.com>
2019-06-18 19:51:07 +0100

    feat(view-specific-advert): Creates a page that allows a user view a specific property [Finishes #166625796]

--- UI/js/hamburger.js
+++ UI/js/hamburger.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('#hamIcon');
 const nav = document.querySelector('.sidenav');
 const exitBtn = document.querySelector('#xBtn');
 const newBtn = document.querySelectorAll('.newbtn');
+const viewSpecificAdvertBtn = document.querySelectorAll('.all-list-btn');
 
 menuBtn.addEventListener('click', () => {
   nav.setAttribute('style', 'display:block !important');
@@ -15,4 +16,9 @@ exitBtn.addEventListener('click', () => {
 const displayUpdatePage = () => {
   window.location.href = './updatelisting.html';
 };
-newBtn.forEach((el) => { el.addEventListener('click', displayUpdatePage); });
+
+const displaySpecifiProperty = () => {
+  window.location.href = './property.html';
+};
+newBtn.forEach(el => el.addEventListener('click', displayUpdatePage));
+viewSpecificAdvertBtn.forEach(el => el.addEventListener('click', displaySpecifiProperty));
