f353cf586e00fedd35a88d8e6ac9862d74aa00fa
Iyara Ferguson <fergusoniyara@gmail.com>
2019-06-19 18:00:22 +0100

    chore(Heroku): Deploy app on heroku [Starts #166797554]

--- API/index.js
+++ API/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
